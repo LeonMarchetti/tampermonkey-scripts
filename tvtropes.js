db07ff46128c0f15dc9993a49c3430a8cbce1d29
Leonardo Marchetti <mleonardoa@gmail.com>
2024-02-17 12:44:59 -0300

    [tvtropes 1.0.1] Added shortcut to ymmv tropes edits

--- tvtropes.js
+++ tvtropes.js
@@ -1,12 +1,12 @@
 // ==UserScript==
 // @name         TvTropes
 // @namespace    http://tampermonkey.net/
-// @version      1.0
+// @version      1.0.1
 // @description  Script for TV Tropes
 // @author       LeonAM
 // @match        https://tvtropes.org/pmwiki/changes.php*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=tvtropes.org
-// @grant        none
+// @grant        GM_registerMenuCommand
 // ==/UserScript==
 /* globals $ */
 
@@ -22,6 +22,10 @@ function hideActorsLinks() {
 
     console.log((count > 0) ? `Hidden ${count} links` : "No links hidden");
 }
+
+GM_registerMenuCommand("Analysis tropes edits", () => {
+    window.location.href = "https://tvtropes.org/pmwiki/changes.php?filter=ymmv";
+});
 
 (function() {
     'use strict';
