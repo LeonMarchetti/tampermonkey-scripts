ac7cf650e07f632737c780e29adf69feb9bc45f5
Leonardo Marchetti <mleonardoa@gmail.com>
2025-08-02 18:14:39 -0300

    [instagram 1.3.0 #20] Added account's summary

--- changelogs/instagram.md
+++ changelogs/instagram.md
@@ -1,5 +1,23 @@
 # Changelog
 
+## [1.3.0] - 02-08-2025
+
+### Agregado
+
+- Obtener resumen de cuenta en formato CSV parseando elementos del DOM
+  - **address**: Saco la dirección completa pero le saco de la primera coma en adelante
+  - **notes**: Saco el texto en gris que está abajo del nombre
+  - **whatsapp**: Saco el link y si empieza con `wa.me/` lo anoto como número de WhatsApp, sino como
+    el enlace del sitio web
+
+| dato             | elemento                                        |
+| ---------------- | ----------------------------------------------- |
+| id               | `h2`                                            |
+| name             | `header :nth-child(4) > div > div > span`       |
+| notes            | `header :nth-child(4) > div > div:nth-child(3)` |
+| address          | `h1`                                            |
+| website/whatsapp | `.x3nfvp2.x193iq5w`                             |
+
 ## [1.2.0] - 18-07-2025
 
 ### Eliminado
--- instagram.js
+++ instagram.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Instagram
 // @namespace    http://tampermonkey.net/
-// @version      1.2.0
+// @version      1.3.0
 // @description  Utilities for Instagram
 // @author       LeonAM
 // @match        https://www.instagram.com/*
@@ -23,6 +23,55 @@
         document.querySelector(`[aria-label="Activar o desactivar audio"]`).click();
     }
 
+    /**
+     * Builds the account summary in CSV format with:
+     *
+     * - Account id
+     * - Name
+     * - Category (with default value)
+     * - Notes
+     * - Address
+     * - Whatsapp number
+     * - Website address
+     * - Today's date in "DD/MM/YYYY" format
+     *
+     * Queries DOM elements to obtain the values
+     *
+     * @returns {string} Account summary in CSV format
+     */
+    function getAccountSummary() {
+        let id = document.querySelector("h2")?.innerText;
+        let name = document.querySelector("header :nth-child(4) > div > div > span")?.innerText;
+        let notes = document.querySelector("header :nth-child(4) > div > div:nth-child(3)")?.innerText || "";
+        let address = document.querySelector("h1")?.innerText.replace(/,.*/, "") || "";
+        let whatsapp = "";
+        let website = "";
+        let date = (new Date()).toLocaleDateString("es-AR", {
+            day: "2-digit",
+            month: "2-digit",
+            year: "numeric"
+        });
+
+        // Search link element and determine either account's website or whatsapp number
+        let link = document.querySelector(".x3nfvp2.x193iq5w")?.innerText || "";
+        if (link) {
+            if (link.startsWith("wa.me")) {
+                // WhatsApp number
+                whatsapp = link.replace("wa.me/", "").replace(/^(2323|11)/, "$1 ");
+            } else {
+                website = link.replace(" y 1 más", "");
+            }
+        }
+
+        return `${id},${name},,"${notes}","${address}",${whatsapp},"${website}",${date}`;
+    }
+
+    function GetSummary() {
+        let accountSummary = getAccountSummary();
+        alert(accountSummary);
+        console.debug(accountSummary);
+    }
+
     document.addEventListener("keyup", e => {
         if (!e.altKey && !e.ctrlKey && !e.shiftKey && e.originalTarget.tagName !== "INPUT") {
             switch (e.code) {
@@ -32,4 +81,5 @@
     });
 
     GM_registerMenuCommand("Mute", ToggleMute);
+    GM_registerMenuCommand("Get Summary", GetSummary);
 })();
