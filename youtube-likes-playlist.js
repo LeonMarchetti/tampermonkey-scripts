54e75a72542fc887934531482c4a618132045abf
Leonardo Marchetti <mleonardoa@gmail.com>
2026-04-19 11:25:56 -0300

    [youtube-likes-playlist 1.1.0] Changed tab for dialog

--- youtube-likes-playlist.js
+++ youtube-likes-playlist.js
@@ -1,12 +1,12 @@
 // ==UserScript==
 // @name         Youtube Likes Playlist
 // @namespace    http://tampermonkey.net/
-// @version      1.0.0
+// @version      1.1.0
 // @description  Utilities for the video likes playlist for Youtube
 // @author       LeonAM
 // @match        https://www.youtube.com/playlist?list=LL
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=youtube.com
-// @grant        none
+// @grant        GM_addStyle
 // ==/UserScript==
 
 /**
@@ -30,6 +30,62 @@ function getVideosList() {
 (function() {
     'use strict';
 
+    GM_addStyle(`
+        /* The Modal (background) */
+        #ypl_modal {
+            display: none; /* Hidden by default */
+            position: fixed; /* Stay in place */
+            z-index: 2202; /* Sit on top */
+            left: 50%;
+            top: 50%;
+            overflow: auto; /* Enable scroll if needed */
+            transform: translate(-50%, -50%);
+        }
+
+        /* Modal Content/Box */
+        #ypl_modal_content {
+            background-color: #fefefe;
+            margin: 15% auto; /* 15% from the top and centered */
+            padding: 1em;
+        }
+
+        /* The Close Button */
+        #ypl_modal_close {
+            color: #aaa;
+            float: right;
+            font-size: 28px;
+            font-weight: bold;
+        }
+
+        #ypl_modal_close:hover, #ypl_modal_close:focus {
+            color: black;
+            text-decoration: none;
+            cursor: pointer;
+        }
+
+        #ypl_modal table, 
+        #ypl_modal th, 
+        #ypl_modal td {
+            border-collapse: collapse;
+            font-family: Ubuntu;
+            padding: 5px;
+            text-align: left;
+            font-size: 18px;
+        }
+
+        #ypl_modal table,
+        #ypl_modal th,
+        #ypl_modal td:nth-child(1) {
+            border: 1px solid;
+        }
+
+        #ypl_modal th,
+        #ypl_modal td:nth-child(1) {
+            background-color: lightgray;
+            font-weight: bold;
+        }
+    `);
+
     const tableStyles = `
         table, th, td {
             border-collapse: collapse;
@@ -49,8 +105,6 @@ function getVideosList() {
     `;
 
     const title = "YouTube Likes Playlist";
-    const header = `<h1>${title}</h1>`;
-    const titleElement = `<title>${title}</title>`;
 
     console.info(`Running Userscript "Youtube Likes Playlist"`);
 
@@ -75,46 +129,90 @@ function getVideosList() {
     /**
      * Builds the HTML table for the videos list
      * @param {{i: number, name: string}[]} list Videos list
+     * @returns {HTMLTableElement} Table HTML element
      */
     function buildTable(list) {
-        const rows = list
-            .map(video => `<tr><td>${video.i}</td><td>${video.name}</td></tr>`)
-            .reduce((out, text) => out + text);
+        const table = document.createElement("table");
+        const thead = document.createElement("thead");
+        const tbody = document.createElement("tbody");
 
-        const table = `<table>
-                <thead><tr><th>N°</th><th>Video</th></tr></thead>
-                <tbody>${rows}</tbody>
-            </table>
-        `;
+        const headerRow = document.createElement("tr");
+        const numberCell = document.createElement("th");
+        const videoCell = document.createElement("th");
+
+        numberCell.textContent = "N°";
+        videoCell.textContent = "Video";
+
+        headerRow.appendChild(numberCell);
+        headerRow.appendChild(videoCell);
+
+        thead.appendChild(headerRow);
+
+        list.forEach(video => {
+            const row = document.createElement("tr");
+            const numberCell = document.createElement("td");
+            const videoCell = document.createElement("td");
+
+            numberCell.textContent = video.i;
+            videoCell.textContent = video.name;
+
+            row.appendChild(numberCell);
+            row.appendChild(videoCell);
+
+            tbody.appendChild(row);
+        });
+
+        table.appendChild(thead);
+        table.appendChild(tbody);
+
         return table;
     }
 
-    /**
-     * Shows the output in a new tab
-     * @param {string} htmlContent 
-     */
-    function showBlob(htmlContent) {
-        const blob = new Blob([htmlContent], { type: 'text/html' });
-        const url = URL.createObjectURL(blob);
-        const w = window.open(url, '_blank');
-        if (!w) {
-            // optionally revoke URL if not used
-            URL.revokeObjectURL(url);
-            return;
-        }
-        // revoke after a short delay so the browser has loaded the blob
-        setTimeout(() => URL.revokeObjectURL(url), 2000);
-    }
-
-    /** Shows the videos list in a new tab */
+    /** Shows the videos list in a dialog */
     function showList() {
         const list = getList();
         const table = buildTable(list);
-        const body = `<body>${header}${table}</body>`;
 
-        const head = `<head><meta charset="utf-8">${titleElement}<style>${tableStyles}</style></head>`;
-        const html = `<!doctype html><html>${head}${body}</html>`;
-        showBlob(html);
+        const modalContent = document.getElementById("ypl_modal_content");
+        const oldTable = modalContent.querySelector("table");
+        if (oldTable) {
+            modalContent.replaceChild(table, oldTable);
+        } else {
+            modalContent.append(table);
+        }
+        
+        document.getElementById("ypl_modal").style.display = "block";
+    }
+
+
+    /**
+     * Builds a modal dialog to show the table
+     * @returns {HTMLDivElement} Modal HTML element
+     */
+    function buildModal() {
+        const close = document.createElement("span");
+        close.id = "ypl_modal_close";
+        close.textContent = "X";
+
+        const header = document.createElement("h1");
+        header.innerText = title;
+
+        const content = document.createElement("div");
+        content.id = "ypl_modal_content";
+        content.append(close, header);
+
+        const modal = document.createElement("div");
+        modal.id = "ypl_modal";
+        modal.append(content);
+
+        return modal;
+    }
+
+    // Add modal to DOM
+    document.querySelector("ytd-app").append(buildModal());
+
+    document.getElementById("ypl_modal_close").onclick = function () {
+        document.getElementById("ypl_modal").style.display = "none";
     }
 
     document.addEventListener("keyup", e => {
